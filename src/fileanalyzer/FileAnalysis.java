4eb348707d5e216d43ee1de11e6237f01a29ce72
urisr131 <uri.israeli@mail.huji.ac.il>
2020-09-09 17:57:16 +0300

    merge

---
+++ src/fileanalyzer/FileAnalysis.java
@@ -0,0 +1,28 @@
+package oop.ex6.fileanalyzer;
+
+import oop.ex6.component.Component;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileAnalysis {
+
+    private static FileAnalysis self = new FileAnalysis();
+
+    private FileAnalysis() {
+    }
+
+    public void Analysis(File toRead) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(toRead))){
+
+            ScopeAnalysis.Analyze(null, reader);
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
