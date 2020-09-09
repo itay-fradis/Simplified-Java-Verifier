5641a1e36428aaf1ce402827dd96d8c12eac1824
itay-fradis <itay.fradis@mail.huji.ac.il>
2020-09-09 15:55:56 +0300

    main class

--- src/main/Sjavac.java
+++ src/main/Sjavac.java
@@ -1,5 +1,39 @@
 package main;
 
 public class Sjavac {
-    int x = 1;
+
+    /** sjava type suffix */
+    private static final String SJAVA_TYPE = ".sjava";
+
+    /** wrong num of args */
+    private static final String WRONG_NUM_OF_ARGS = "USAGE: wrong num of args";
+
+    /** file not of type 'sjava' */
+    private static final String SJAVA_FILE_WARNING = "WARNING: Running on a non s-java file";
+
+    /** valid num of args */
+    private static final int NUM_OF_ARGS = 1;
+
+    /***/
+    private static final int CODE_IS_LEGAL = 0;
+
+    /***/
+    private static final int CODE_IS_ILLEGAL = 1;
+
+    /***/
+    private static final int IO_ERROR_CODE = 2;
+
+
+    public static void main(String[] args){
+        if (args.length != NUM_OF_ARGS){
+            System.err.println(WRONG_NUM_OF_ARGS);
+            System.exit(1);
+        }
+        String fileName = args[0];
+        if (!fileName.endsWith(SJAVA_TYPE)){
+            System.out.println(IO_ERROR_CODE);
+            System.out.println(SJAVA_FILE_WARNING);
+        }
+
+    }
 }
