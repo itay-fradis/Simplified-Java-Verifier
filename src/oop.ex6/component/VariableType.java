a4b3726f05eeb08e3854694f1eeabf43b6fb9198
urisr131 <uri.israeli@mail.huji.ac.il>
2020-09-10 12:27:54 +0300

    update

--- src/oop.ex6/component/VariableType.java
+++ src/oop.ex6/component/VariableType.java
@@ -1,8 +1,25 @@
 package oop.ex6.component;
 
-/***
- *
- */
-public class VariableType {
+import java.util.HashMap;
+import java.util.Map;
 
+public enum VariableType {
+
+    INT("int"),
+
+    String("String");
+
+    private static final Map<String, VariableType> map = new HashMap<>();
+
+    private final String type;
+
+    static {
+        for (VariableType v: values()){
+            map.put(v.name(), v);
+        }
+    }
+
+    VariableType(final String type) {this.type = type;}
+
+    public static VariableType getType(String type){return map.get(type);}
 }
