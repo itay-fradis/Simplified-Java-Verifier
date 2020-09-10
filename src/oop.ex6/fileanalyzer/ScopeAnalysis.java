edd68c2bb7b9e0719e916fb95d9269c96f9748a2
urisr131 <uri.israeli@mail.huji.ac.il>
2020-09-10 11:12:41 +0300

    update

--- src/oop.ex6/fileanalyzer/ScopeAnalysis.java
+++ src/oop.ex6/fileanalyzer/ScopeAnalysis.java
@@ -1,51 +1,64 @@
 package oop.ex6.fileanalyzer;
 
-import oop.ex6.classification.Classifier;
+import oop.ex6.classification.LineClassification;
 import oop.ex6.classification.LineType;
+import oop.ex6.component.Component;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+
 
 /**
  * Analyze a scope in file
  */
 public class ScopeAnalysis {
-//    /**
-//     * scope analyzer constructor
-//     */
-//    private ScopeAnalysis(){};
-//    /**
-//     * static member of analyzer
-//     */
-//    private static ScopeAnalysis analyzer = new ScopeAnalysis();
-//
-//    /**
-//     * a private class represent a scope
-//     */
-//    private class Scope{
-//        private ArrayList<Component> components;
-//    }
-
-//    /**
-//     * a deque of scopes
-//     */
-//    private Scope[] scopes;
 
     /**
-     * get row after
-     * @param fileName
-     * @return
-     * @throws IOException
+     * scope analyzer constructor
      */
-    public static boolean analyze(String fileName) throws IOException {
+    private ScopeAnalysis(){};
+    /**
+     * static member of analyzer
+     */
+    private static ScopeAnalysis analyzer = new ScopeAnalysis();
+
+    /**
+     * a private class represent a scope
+     */
+    private class Scope{
+        private ArrayList<Component> components;
+    }
+
+    /**
+     * a deque of scopes
+     */
+    private Scope[] scopes;
+
+
+    public static void Analyze(Scope[] s, BufferedReader reader) throws IOException {
         String line;
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
         while ((line = reader.readLine()) != null){
-            line = line.trim();
-            if (!line.matches("^int\\s+[a-z];"))
-                return false;
+            LineType type = LineClassification.generalClassify(line);
+
+            switch (type){
+                case NORMAL_LINE:
+                    type = LineClassification.SemiColonClassify(line);
+                    switch (type){
+                        case NEW_VARIABLE:
+                            //// component factory
+                            break;
+                    }
+                    break;
+                case OPEN_SCOPE_LINE:
+                    break;
+                case CLOSED_SCOPE_LINE:
+                    break;
+                case BAD_LINE:
+                    throw new RuntimeException("test");
+
             }
-        return true;
         }
+
+    }
 }
