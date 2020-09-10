66ab669e711b5b1dcab35dc2a152268bd8a8a298
urisr131 <uri.israeli@mail.huji.ac.il>
2020-09-10 10:19:51 +0300

    component opdate

--- src/oop.ex6/component/Component.java
+++ src/oop.ex6/component/Component.java
@@ -1,5 +1,44 @@
-package component;
+package oop.ex6.component;
 
+/**
+ * an abstract class represents a component
+ */
 public class Component {
+    /** type of component */
+    private final VariableType type = null;
+    /** name of component */
+    private final String name;
+    /** is component final*/
+    private final boolean isFinal = false;
 
+    /**
+     * constructor for component
+     * @param t - type
+     * @param n  - name
+     * @param f - is final
+     */
+    public Component(String t, String n, String f){
+        name = n;
+    }
+
+    /**
+     * @return type
+     */
+    public VariableType getType() {
+        return type;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * @return is final
+     */
+    public boolean isFinal() {
+        return isFinal;
+    }
 }
